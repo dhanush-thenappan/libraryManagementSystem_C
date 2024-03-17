f3c230d1a6c64e2d7da820678d7835fbfb029fed
Dhanush Thenappan <dhanushthenappan@gmail.com>
2024-03-17 18:38:26 +0530

    Create Admin verification

--- src/libraryManagementSystem.c
+++ src/libraryManagementSystem.c
@@ -5,10 +5,66 @@
 // User header files
 #include "libraryManagementSystem.h"
 
-int main(){
-    greetUser();
+int main() {
+    char userType;
+    printf("| Welcome to Library Management System\n");
+    UserTypeCheck:
+    printf("| Are you [A]dmin or [U]ser : ");
+    scanf("%c", &userType);
+    switch(userType) {
+        case 'A':
+            admin();
+            break;
+        case 'a':
+            admin();
+            break;
+        case 'U':
+            user();
+            break;
+        case 'u':
+            user();
+            break;
+        default:
+            printf("Enter a valid user type\n");
+            getchar();
+            goto UserTypeCheck;
+    }
+    return 0;
 }
 
-void greetUser() {
-    printf("|%30s|", "Welcome");
+void admin() {
+    char *adminName = (char*)malloc(sizeof(ADMINNAME)+1);
+    char *adminPassword = (char*)malloc(sizeof(ADMINPASSWORD)+1);
+    int loginAttempt = 1;
+    while(loginAttempt <= MAXLOGINATTEMPT)
+    {
+        printf("\nEnter admin name : ");
+        scanf("%s", adminName);
+        if(!strncmp(adminName, ADMINNAME, sizeof(ADMINNAME))){
+            printf("| Welcome %s\n", ADMINNAME);
+            break;
+        }
+        else {
+            printf("| Wrong admin name. Please try again. %d attempts left\n", MAXLOGINATTEMPT - loginAttempt++);
+        }
+    }
+    while(loginAttempt <= MAXLOGINATTEMPT)
+    {
+        printf("\nEnter admin password : ");
+        scanf("%s", adminPassword);
+        if(!strncmp(adminPassword, ADMINPASSWORD, sizeof(ADMINPASSWORD))){
+            printf("| Password Correct\n");
+            break;
+        }
+        else {
+            printf("| Wrong admin password. Please try again. %d attempts left\n", MAXLOGINATTEMPT - loginAttempt++);
+        }
+    }
+
+    free(adminName);
+    free(adminPassword);
+}
+
+void user() {
+
 }
