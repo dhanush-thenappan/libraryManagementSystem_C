cf5315141c8c3147667afd0895d99bb88fc46d7c
Dhanush Thenappan <dhanushthenappan@gmail.com>
2024-03-18 11:27:54 +0530

    Create UI for book management

---
+++ src/Admin/bookManagement.c
@@ -0,0 +1,38 @@
+// Built-in header files
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+// User header files
+#include "../libraryManagementSystem.h"
+#include "./admin.h"
+#include "./bookManagement.h"
+
+void bookManagement() {
+    char *choice = (char*)malloc(sizeof(char));
+    printf("\e[1;1H\e[2J");
+    printf("| Welcome to Book management\n");
+    BMChoiceCheck:
+    printf("| [C]reate book\n| [U]pdate book\n| [D]elete book\n| [R]ead details of a book\n| Your options : ");
+    scanf("%c", choice);
+    
+    if(*choice > 97 && *choice < 123){
+        *choice -= 32;
+    }
+    switch(*choice) {
+        case 'C':
+            break;
+        case 'U':
+            break;
+        case 'D':
+            break;
+        case 'R':
+            break;
+        default:
+            printf("\e[1;1H\e[2J");
+            printf("| Enter a valid Choice\n");
+            getchar();
+            goto BMChoiceCheck;
+            break;
+    }
+    free(choice);
+}
---
+++ src/Admin/bookManagement.h
@@ -0,0 +1,7 @@
+#ifndef BOOKMANAGEMENT_H
+
+#define BOOKMANAGEMENT_H
+
+void bookManagement(void);
+
+#endif
